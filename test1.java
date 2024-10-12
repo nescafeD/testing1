7f755ae85b1a6e32e712c87d3762fa7c3ba7be05
Deepansh Gupta <deepansh.gupta@techolution.com>
2024-10-12 16:59:31 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,22 +1,22 @@
 package com.kingyu.flappybird.app;
 
 import com.kingyu.flappybird.component.GameElementLayer;
-// import com.kingyu.flappybird.component.Bird;
-// import com.kingyu.flappybird.component.GameBackground;
-// import com.kingyu.flappybird.component.GameForeground;
-// import com.kingyu.flappybird.component.WelcomeAnimation;
+import com.kingyu.flappybird.component.Bird;
+import com.kingyu.flappybird.component.GameBackground;
+import com.kingyu.flappybird.component.GameForeground;
+import com.kingyu.flappybird.component.WelcomeAnimation;
 
-// import static com.kingyu.flappybird.util.Constant.FRAME_HEIGHT;
-// import static com.kingyu.flappybird.util.Constant.FRAME_WIDTH;
-// import static com.kingyu.flappybird.util.Constant.FRAME_X;
+import static com.kingyu.flappybird.util.Constant.FRAME_HEIGHT;
+import static com.kingyu.flappybird.util.Constant.FRAME_WIDTH;
+import static com.kingyu.flappybird.util.Constant.FRAME_X;
 import static com.kingyu.flappybird.util.Constant.FRAME_Y;
 import static com.kingyu.flappybird.util.Constant.FPS;
 import static com.kingyu.flappybird.util.Constant.GAME_TITLE;
 
 import java.awt.Frame;
-import java.awt.Graphics;
-import java.awt.event.*;
-import java.awt.image.BufferedImage;
+// import java.awt.Graphics;
+// import java.awt.event.*;
+// import java.awt.image.BufferedImage;
 
 
 /**
